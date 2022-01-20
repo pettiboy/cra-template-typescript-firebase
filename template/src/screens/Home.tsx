71c0a8b974df70bcce2a3c89da19d8e852b4aa9e
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2022-01-20 21:32:43 +0530

    🚀 RELEASE: version 0.0.4

--- template/src/screens/Home.tsx
+++ template/src/screens/Home.tsx
@@ -1,9 +1,6 @@
 import { useEffect } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useNavigate } from "react-router-dom";
 import Logout from "../components/auth/Logout";
 import Center from "../components/utils/Center";
-import { auth } from "../config/firebase";
 
 interface Props {}
 
